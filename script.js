b6350e41a352d8ad053024ef896011bfe76562ef
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-27 19:03:37 +0700

    add animation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
+    let kotak = document.getElementById("menu");
+
     document.querySelector(".menu").classList.toggle("active");
 }
 
@@ -6,6 +8,10 @@ function toggleMenu() {
     let menu = document.querySelector(".menu");
     menu.classList.toggle("active");
 }
+
+setTimeout(() => {
+    menu.classList.add("muncul");
+  }, 10);
 
 // Menutup menu ketika salah satu item diklik
 document.querySelectorAll(".menu a").forEach(item => {
@@ -17,8 +23,13 @@ document.querySelectorAll(".menu a").forEach(item => {
 function kirim() {
     let notif = document.getElementById("notif");
 
-    notif.style.display="flex";
+    // Hapus animasi sebelumnya jika ada
+    notif.classList.remove("flex-show");
+    void notif.offsetWidth; // reflow agar animasi bisa diulang
+
+    notif.classList.add("flex-show");
 }
+
 
 function tutup() {
     let notif = document.getElementById("notif");
